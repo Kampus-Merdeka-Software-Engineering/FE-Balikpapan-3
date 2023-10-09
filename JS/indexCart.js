dbe199437b321126466dff78db8ccbbd12a44457
SoviaIndrawati <143269063+SoviaIndrawati@users.noreply.github.com>
2023-10-09 12:24:15 +0700

    Update indexCart.js

--- JS/indexCart.js
+++ JS/indexCart.js
@@ -18,109 +18,109 @@ let products = [
   {
     id: 1,
     name: "Sambal Nagih - Cakalang",
-    image: "1.PNG",
+    image: "1.png",
     price: 37000,
   },
   {
     id: 2,
     name: "Cuanki Kriboow",
-    image: "2.PNG",
+    image: "2.png",
     price: 14500,
   },
   {
     id: 3,
     name: "Cireng Gochujang",
-    image: "3.PNG",
+    image: "3.png",
     price: 17400,
   },
   {
     id: 4,
     name: "Baso Goreng Marenta",
-    image: "4.PNG",
+    image: "4.png",
     price: 16500,
   },
   {
     id: 5,
     name: "Baso Aci Tulangrangu",
-    image: "5.PNG",
+    image: "5.png",
     price: 15900,
   },
   {
     id: 6,
     name: "Rabbokki by Kylafood",
-    image: "6.PNG",
+    image: "6.png",
     price: 21600,
   },
   {
     id: 7,
     name: "Abon Ikan Makku",
-    image: "7.PNG",
+    image: "7.png",
     price: 32000,
   },
   {
     id: 8,
     name: "Zanana Chips",
-    image: "8.PNG",
+    image: "8.png",
     price: 15400,
   },
   {
     id: 9,
     name: "Batagor Chips",
-    image: "9.PNG",
+    image: "9.png",
     price: 17500,
   },
   {
     id: 10,
     name: "Kaula Keripik Cireng",
-    image: "10.PNG",
+    image: "10.png",
     price: 14300,
   },
   {
     id: 11,
     name: "Ciomy Cuanki",
-    image: "11.PNG",
+    image: "11.png",
     price: 14999,
   },
   {
     id: 12,
     name: "Seblak Coet Marenta",
-    image: "12.PNG",
+    image: "12.png",
     price: 12500,
   },
   {
     id: 13,
     name: "Seonju/Minuman Soju",
-    image: "13.PNG",
+    image: "13.png",
     price: 31900,
   },
   {
     id: 14,
     name: "Makro Nich Marenta",
-    image: "14.PNG",
+    image: "14.png",
     price: 19000,
   },
   {
     id: 15,
     name: "Astro Goods - Crispy Brownies",
-    image: "15.PNG",
+    image: "15.png",
     price: 14800,
   },
   {
     id: 16,
     name: "Fanta Soda Drink",
-    image: "16.PNG",
+    image: "16.png",
     price: 45000,
   },
   {
     id: 17,
     name: "Mujigae Jajangmyeon",
-    image: "17.PNG",
+    image: "17.png",
     price: 20400,
   },
   {
     id: 18,
     name: "Mujigae Banana Milk",
-    image: "18.PNG",
+    image: "18.png",
     price: 10200,
   },
 ];
@@ -157,7 +157,7 @@ function reloadCard() {
     if (value != null) {
       let newDiv = document.createElement("li");
       newDiv.innerHTML = `
-        <div><img src="img/menu/${value.image}"/></div>
+        <div><img src="img/Menu/${value.image}"/></div>
         <div>${value.name}</div>
         <div>${value.price.toLocaleString()}</div>
         <div>
